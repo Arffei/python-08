afd5dcb5b0277c6eb818d9418fe6cf9dec2d80c8
Arffei <arffei.storm@gmail.com>
2026-08-16 14:09:25 +0300

    666

--- main1.py
+++ main1.py
@@ -1,1 +1,10 @@
-+
+n = int(input())
+arr = []
+
+for i in range(n):
+    arr.append(int(input()))
+
+arr.reverse()
+
+for x in arr:
+    print(x)
--- main2.py
+++ main2.py
@@ -1,1 +1,5 @@
-+
+n = int(input())
+arr = list(map(int, input().split()))
+
+result = [arr[-1]] + arr[:-1]
+print(*result)
