980e2457420f68704720620c619d4c2e2129e8d6
Arffei <arffei.storm@gmail.com>
2026-08-16 14:09:51 +0300

    666

--- main3.py
+++ main3.py
@@ -1,1 +1,22 @@
-+
+m = int(input())
+n = int(input())
+weights = []
+
+for i in range(n):
+    weights.append(int(input()))
+
+weights.sort()
+left = 0
+right = n - 1
+boats = 0
+
+while left <= right:
+    if left == right:
+        boats += 1
+        break
+    if weights[left] + weights[right] <= m:
+        left += 1
+    right -= 1
+    boats += 1
+
+print(boats)
